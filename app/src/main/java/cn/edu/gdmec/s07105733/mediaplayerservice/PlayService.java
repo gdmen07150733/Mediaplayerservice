1fbcecf3b776ebe14fe3f3d77e49fca1a384cf4d
gdmen07150733 <413359404>
2016-11-21 00:12:50 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07105733/mediaplayerservice/PlayService.java
@@ -0,0 +1,35 @@
+package cn.edu.gdmec.s07105733.mediaplayerservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.UnsupportedSchemeException;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2016/11/17 0017.
+ */
+public class PlayService extends Service{
+    MediaPlayer mediaPlayer;
+    public PlayService(){
+    }
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    public int onStartCommand(Intent intent,int flags,int startId){
+        mediaPlayer.start();
+        return super.onStartCommand(intent,flags,startId);
+    }
+    public void onCreate(){
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this,R.raw.music);
+        mediaPlayer.setLooping(true);
+    }
+    public void onDestory(){
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+}
+
